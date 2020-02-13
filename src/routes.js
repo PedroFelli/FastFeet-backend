e2060b93d41547115b8ca398ca2d350e0a29edc6
Pedro Fellipe Mendes Amaral - Analista de Sistemas <pedro.amaral@unievangelica.edu.br>
2020-02-13 16:08:30 -0300

    token

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,17 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.post('/user', UserController.store);
-routes.put('/user', UserController.update);
+
 routes.post('/sessions', SessionController.store);
+routes.post('/user', UserController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/user', UserController.update);
+
 
 export default routes;
